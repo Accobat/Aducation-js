ef94905b958f44288cf5b32a3b4e429315567015
Podrugin Alexey <podrugin@hotmail.com>
2022-11-01 00:14:49 +0300

    Add files via upload

---
+++ Numb.js
@@ -0,0 +1,29 @@
+// #1 Получить число pi и округлить его до 2-х знаков после точки
+
+// const pi = Number(Math.PI.toFixed(2));
+// console.log(pi);
+
+//#2 найти максимальное и минимальное числа ряда
+
+// const minNum = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+// const maxNum = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+// console.log(minNum, maxNum);
+
+//#3 Получить случайное число и округлить его до двух цифр после запятой. Получить случайное целое число от 0 до любого произвольного числа
+
+// const random = Number(Math.random().toFixed(2));
+// console.log(random);
+
+// const X = 20;
+// const randomX = Math.round(Math.random() * X);
+// console.log(randomX);
+
+//#4 Привести к нормальному виду 0.6+0.7
+
+// const fixNum = Number(0.6 + 0.7).toFixed(1);
+// console.log(fixNum);
+
+//#5 получить число из строки
+
+// const numFromStr = parseFloat("100$");
+// console.log(numFromStr);
