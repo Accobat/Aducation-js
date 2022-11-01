c5dd4daa3317a20afde3446eb554b6655d038b90
Podrugin Alexey <podrugin@hotmail.com>
2022-11-01 14:51:46 +0300

    Add files via upload

---
+++ object.js
@@ -0,0 +1,16 @@
+// Создать объект с полем product, равным ‘iphone’
+
+// const item = {
+//     product: "iPhone"
+// }
+
+// Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+
+// item.price = 1000;
+// item.currency = "dollar";
+
+// Добавить поле details, которое будет содержать объект с полями model и color
+
+// item.details = {};
+// item.details.model = "XR";
+// item.details.color = "Red";
