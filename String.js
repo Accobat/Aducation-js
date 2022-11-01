72e03b2ab08cd888bb9cdc47659cdbd1e7a9a69b
Podrugin Alexey <podrugin@hotmail.com>
2022-11-01 11:04:24 +0300

    Add files via upload

---
+++ String.js
@@ -0,0 +1,44 @@
+// Получить первую и последнюю буквы строки
+
+// const firstChar = string[0];
+// const lastChar = string[string.length - 1];
+// console.log(firstChar, lastChar);
+
+// Сделать первую и последнюю буквы в верхнем регистре
+
+// const firstLastUpper = `${string[0].toUpperCase()}${string.slice(1,-1)}${string[string.length - 1].toUpperCase()}`;
+// console.log(firstLastUpper);
+
+// Найти положение слова ‘string’ в строке
+
+// const wordPos = string.indexOf('string');
+// console.log(wordPos);
+
+// Найти положение второго пробела (“вручную” ничего не считать)
+
+// const firstSpace = string.indexOf(' ');
+// const secondSpace = string.indexOf(' ', firstSpace + 1);
+// console.log(secondSpace);
+
+// Получить строку с 5-го символа длиной 4 буквы
+
+// const substr = string.substr(5, 4);
+// console.log(substr);
+
+// Получить строку с 5-го по 9-й символы
+
+// const slice = string.slice(5, 10);
+// console.log(slice)
+
+// Получить новую строку из исходной путем удаления последних 6-и символов (то есть исходная строка без последних 6и символов)
+
+
+// const lastSix = string.slice(0, -6);
+// console.log(lastSix)
+
+// Из двух переменных a=20 и b=16 получить переменную string, в которой будет содержаться текст “2016”
+
+
+// const a = 20, b = 16;
+// const strFromNum = String(a) + String(b);
+// console.log(strFromNum);
